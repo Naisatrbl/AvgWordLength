7e99d7732b0f5061957b9fca3b17a138c27aa2c7
Christopher Duong <cduong1@lsu.edu>
2021-10-30 17:09:18 -0500

    Updated ReadMe and configured main from default

--- src/AvgWordLength.java
+++ src/AvgWordLength.java
@@ -1,5 +1,40 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+
+public class AvgWordLength {
+
+  public static void main(String[] args) throws Exception {
+
+
+    if (args.length != 2) {
+      System.out.printf("Usage: AvgWordLength <input dir> <output dir>\n");
+      System.exit(-1);
     }
+
+    Job job = new Job();
+    
+    job.setJarByClass(AvgWordLength.class);
+    
+    job.setJobName("Average Word Length");
+
+    FileInputFormat.setInputPaths(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    
+    job.setMapperClass(LetterMapper.class);
+	job.setReducerClass(AverageReducer.class);
+	
+	job.setMapOutputKeyClass(Text.class);
+	job.setMapOutputValueClass(IntWritable.class);
+	
+	job.setOutputKeyClass(Text.class);
+	job.setOutputValueClass(DoubleWritable.class);
+    
+    boolean success = job.waitForCompletion(true);
+    System.exit(success ? 0 : 1);
+  }
 }
